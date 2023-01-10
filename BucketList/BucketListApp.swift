25fc359f9e672d1a914d56e4c0cab10ac8e239bf
choijun0 <cjy00050005@gmail.com>
2023-01-10 14:55:20 +0900

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by 최준영 on 2023/01/10.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
